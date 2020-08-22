c9034874d426910d2f45491dc9e7954e046085ec
IvanTepes <ivan.tepes@outlook.com>
2020-08-22 11:46:08 +0000

    added bacd background options in settings added images and jquery function to change styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,8 +18,7 @@ $('.card').on('click', function() {
 });
  */
 
-/* Main Menu buttons*/
-
+/* Main Menu Options Buttons*/
  $('.hold').click(function() {
      $(this).toggleClass("hold-active");
  });
@@ -47,16 +46,14 @@ $('.new-game-button').click(function() {
  $('.settings-main-menu-back-button').click(function() {
      $('.settings-menu-screen').addClass('hidden');
  });
- 
-/* $('.setting-theme-green').click(function() {
-     $('.settings-menu-screen, .main-menu-container, .game-screen').addClass("theme-green");
- }); */
+ /* /main menu options buttons*/
 
+/* Settings Menu Background selection */
  $('.setting-theme-green').on('click',function() {
     if($(".game-screen").hasClass("game-screen") || ("theme-yellow") || ("theme-red")) {
-        $("#game").removeClass("game-screen").addClass("theme-green");
-        $("#menu-container").removeClass("theme-red").addClass("theme-green");
-        $("#settings-container").removeClass("theme-red").addClass("theme-green");
+        $("#game").removeClass("game-screen").removeClass("theme-yellow").removeClass("theme-red").addClass("theme-green");
+        $("#menu-container").removeClass("theme-red").removeClass("theme-yellow").removeClass("theme-red").addClass("theme-green");
+        $("#settings-container").removeClass("theme-yellow").removeClass("theme-red").removeClass("theme-red").addClass("theme-green");
     }
 });
 
@@ -75,6 +72,32 @@ $('.setting-theme-red').on('click',function() {
         $("#settings-container").removeClass("theme-yellow").removeClass("theme-green").removeClass("theme-red").addClass("theme-red");
     } 
 });
+/* /settings menu background selection */
+
+/* Settings Menu Card Background Selection */
+$('.settings-card-back-red').on('click',function() {
+    if($(".card-face--front").hasClass("card-face--front") || ("card-theme-red") ||       
+    ("card-theme-blue") || ("card-theme-black")) {
+        $(".card-face--front").removeClass("card-theme-black").removeClass("card-theme-blue").removeClass("card-theme-red").addClass("card-theme-red");
+    } 
+});
+
+$('.settings-card-back-blue').on('click',function() {
+    if($(".card-face--front").hasClass("card-face--front") || ("card-theme-red") ||       
+    ("card-theme-blue") || ("card-theme-black")) {
+        $(".card-face--front").removeClass("card-theme-black").removeClass("card-theme-red").removeClass("card-theme-blue").addClass("card-theme-blue");
+    } 
+});
+
+$('.settings-card-back-black').on('click',function() {
+    if($(".card-face--front").hasClass("card-face--front") || ("card-theme-red") ||       
+    ("card-theme-blue") || ("card-theme-black")) {
+        $(".card-face--front").removeClass("card-theme-black").removeClass("card-theme-red").removeClass("card-theme-blue").addClass("card-theme-black");
+    } 
+});
+/* /settings menu card background selection */
+
+
  /////////////////////////////////////////////////////////////////////////////////////////
 /* working script */
 
